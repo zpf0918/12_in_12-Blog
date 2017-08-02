767f2f22b4ed0147a8eb9784870d228c02ea3c45
zpf0918 <zhanggongzifei@163.com>
2017-08-02 13:59:50 +0800

    add users

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(params[:comment].permit(:title, :body))
@@ -12,5 +13,5 @@ class CommentsController < ApplicationController
     @comment.destroy
     redirect_to post_path(@post)
   end
-  
+
 end
